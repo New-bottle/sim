136464922e55d3cf645b26d0df4a99a5e1ddcf8b
New-bottle <775712558@qq.com>
2018-02-05 20:33:26 +0800

    readin

--- src/main.py
+++ src/main.py
@@ -16,41 +16,70 @@ with open(test_file, 'rb') as f:
 print '-------------------LOAD END----------------------'
 
 input_num = 2+5+4+32+32
-learning_rate = 0.99
+learning_rate = 0.10
 iterations = 1000
-batch_size = 1000
-drop_keep_prob = 0.99
-hid1 = 100
+batch_size = 100000
+drop_keep_prob = 0.90
+hid1 = 200
+hid2 = 100
 output = 1
 
 X_data = []
 for each in X_tmp:
 	tmp5 = np.zeros(5)
 	tmp4 = np.zeros(4)
-	tmp321 = np.zeros(32)
-	tmp322 = np.zeros(32)
+	tmp32_1 = np.zeros(32)
+	tmp32_2 = np.zeros(32)
 	tmp5[int(each[1]) - 1] = 1
 	tmp4[int(each[2]) - 1] = 1
-	tmp321[int(each[3]) - 1] = 1
-	tmp322[int(each[4]) - 1] = 1
+	tmp32_1[int(each[3]) - 1] = 1
+	tmp32_2[int(each[4]) - 1] = 1
 	tmp = [each[0], each[5]]
 	tmp.extend(tmp5)
 	tmp.extend(tmp4)
-	tmp.extend(tmp321)
-	tmp.extend(tmp322)
+	tmp.extend(tmp32_1)
+	tmp.extend(tmp32_2)
 	X_data.append(tmp)
 
 X_data = np.array(X_data)
+Y_data = np.array(Y_data)
+Y_data = Y_data.reshape(100000,1)
 # print X_data[0]
+L = [input_num, 200, 100, 1]
+LAYERS = len(L) - 1
 
 keep_prob = tf.placeholder("float")
-w1 = tf.Variable(tf.random_normal([input_num, hid1], stddev=1))
-w2 = tf.Variable(tf.random_normal([hid1,output], stddev=1))
-b1 = tf.Variable(tf.zeros([1, hid1])+0.1)
+x = tf.placeholder(tf.float32, shape=[None,input_num], name="input")
+def weight_variable(shape):
+	initial = tf.random_normal(shape, stddev = 0.1)
+	return tf.Variable(initial)
+def bias_variable(shape):
+	initial = tf.constant(0.1, shape = shape)
+	return tf.Variable(initial)
+
+W = list(range(LAYERS + 1))
+b = list(range(LAYERS + 1))
+yt = list(range(LAYERS+ 1))
+yt[0] = x
+for i in range(LAYERS):
+	W[i + 1] = weight_variable([L[i], L[i + 1]])
+	b[i + 1] = bias_variable([L[i + 1]])
+
+for i in range(LAYERS):
+	if i == LAYERS - 1:
+		yt[i] = tf.nn.dropout(yt[i], keep_prob)
+	yt[i + 1] = tf.nn.sigmoid(tf.matmul(yt[i], W[i + 1]) + b[i + 1])
+y = tf.nn.softmax(yt[LAYERS])
+'''
+w1 = tf.Variable(tf.random_normal([input_num, hid1]))
+w2 = tf.Variable(tf.random_normal([hid1,output]    ))
+w3 = tf.Variable(tf.random_normal([
+b1 = tf.Variable(tf.zeros([1, hid1])  +0.1)
 b2 = tf.Variable(tf.zeros([1, output])+0.1)
-x  = tf.placeholder(tf.float32, shape=(None,input_num), name="input")
-a = tf.nn.relu(tf.matmul(x, w1) + b1)
-y = tf.nn.relu(tf.matmul(a, w2) + b2)
+
+a = tf.nn.sigmoid(tf.matmul(x, w1) + b1)
+y = tf.matmul(a, w2) + b2
+'''
 y_ = tf.placeholder(tf.float32, shape=(None, 1), name = "label")
 
 loss = tf.reduce_mean(tf.reduce_sum(tf.square(y_ - y), reduction_indices=[1]))
@@ -72,12 +101,26 @@ X_val, y_val = new_batch(1000)
 def mape(y_true, y_pred):
 	return tf.reduce_mean(tf.reduce_sum(abs(y_true-y_pred)/y_true))
 
-for i in range(iterations):
-	batch_x, batch_y_ = new_batch(batch_size)
-	sess.run(train_step, feed_dict = {x: batch_x, y_: batch_y_, keep_prob:drop_keep_prob})
-	if i % (iterations // 100) == 0:
-		print ('Process: {}%'.format((i // (iterations // 100) + 1) * 1))
-		print sess.run(y_[0], feed_dict = {x: batch_x, y_: batch_y_, keep_prob:drop_keep_prob})
-		print sess.run(y[0], feed_dict = {x: batch_x, y_: batch_y_, keep_prob:drop_keep_prob})
-		correct_prediction = mape(y_, y)
-		print ('MAPE:', sess.run(correct_prediction, feed_dict = {x: X_val, y_: y_val, keep_prob: 1.0}))
+BATCH = False
+if BATCH:
+	for i in range(iterations):
+		batch_x, batch_y_ = new_batch(batch_size)
+		sess.run(train_step, feed_dict = {x: batch_x, y_: batch_y_, keep_prob: drop_keep_prob})
+		if i % (iterations // 10) == 0:
+			print ('Process: {}%'.format((i // (iterations // 10) + 1) * 10))
+			print 'y_:', sess.run(y_[0], feed_dict = {x: batch_x, y_: batch_y_, keep_prob: 1})
+			print 'y:',  sess.run(y[0],  feed_dict = {x: batch_x, y_: batch_y_, keep_prob: 1})
+			print 'loss:', sess.run(loss,feed_dict = {x: batch_x, y_: batch_y_, keep_prob: 1})
+			correct_prediction = mape(y_, y)
+			print ('MAPE:', sess.run(correct_prediction, feed_dict = {x: X_val, y_: y_val, keep_prob: 1}))
+else:
+	for i in range(iterations):
+		sess.run(train_step, feed_dict = {x: X_data, y_: Y_data, keep_prob: drop_keep_prob})
+		if i % (iterations // 100) == 0:
+			#			print 'w1:', sess.run(w1[0], feed_dict = {x: X_data, y_: Y_data})
+			print ('Process: {}%'.format((i // (iterations // 100) + 1) * 1))
+			print 'y_:', sess.run(y_[0], feed_dict = {x: X_data, y_: Y_data, keep_prob: 1})
+			print 'y:',  sess.run(y[0],  feed_dict = {x: X_data, y_: Y_data, keep_prob: 1})
+			print 'loss:', sess.run(loss,feed_dict = {x: X_data, y_: Y_data, keep_prob: 1})
+			correct_prediction = mape(y_, y)
+			print ('MAPE:', sess.run(correct_prediction, feed_dict = {x: X_val, y_: y_val, keep_prob: 1.0}))
--- src/readin.py
+++ src/readin.py
@@ -7,14 +7,15 @@ training_file = "../data/training_data.csv"
 test_file = "../data/test_data.csv"
 
 def get_training_set(filename):
-	ans = []
+	X = []
+	y = []
 	with open(filename, 'rb') as f:
 		csv_reader = csv.DictReader(f)
 		for row in csv_reader:
-			ans.append([int(row['Volume']), int(row['Good Type']), int(row['Business Type']),
-				int(row['Start Business ID']), int(row['End Business ID']), float(row['Distance']),
-				float(row['Price'])])
-	return ans
+			X.append([int(row['Volume']), int(row['Good Type']), int(row['Business Type']),
+				int(row['Start Business ID']), int(row['End Business ID']), float(row['Distance'])])
+			y.append(float(row['Price']))
+	return [X, y]
 
 def get_test_set(filename):
 	ans = []
@@ -25,9 +26,10 @@ def get_test_set(filename):
 				int(row['Start Business ID']), int(row['End Business ID']), float(row['Distance'])])
 	return ans
 if __name__=='__main__':
-	training_data = get_training_set(training_file)
+	[X,y] = get_training_set(training_file)
 	with open('../data/training.pk1', 'wb') as f:
-		pickle.dump(training_data, f)
+		pickle.dump(X, f)
+		pickle.dump(y, f)
 	test_data = get_test_set(test_file)
 	with open('../data/test.pk1', 'wb') as f:
 		pickle.dump(test_data, f)
