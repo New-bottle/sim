c6c0665696ac52dfef1387ea756963fe28a8651b
WantonWind <875357936@qq.com>
2018-02-07 21:56:34 +0800

    add file-support

--- src/sess.py
+++ src/sess.py
@@ -6,11 +6,6 @@ import os
 
 print ('------------------LOAD START---------------------')
 
-# train_session_folder = '../models/'
-# train_session_name = '1'
-# train_session_path = train_session_folder + train_session_name
-# train_graph_path = train_session_folder + train_session_name + "-20.meta"
-
 train_file = '../data/training.pk1'
 test_file = '../data/test.pk1'
 
@@ -58,24 +53,24 @@ print(Y_data.shape)
 print ('-------------------LOAD END----------------------')
 
 np.set_printoptions(suppress=True)
-X_data = X_data[0:10]
-Y_data = Y_data[0:10]
+X_data = X_data[0:100]
+Y_data = Y_data[0:100]
 print(X_data.shape)
-print(X_data)
+# print(X_data)
 print(Y_data.shape)
-print(Y_data * 1992)
+# print(Y_data * 1992)
 
 input_num = 2+5+4+32+32
 hid1 = 100
-# hid2 = 100
+# hid2 = 50
 output_num = 1
 
-learning_rate = 0.05
+learning_rate = 0.01
 
 # 需要模改
 def add_layer(inputs, in_size, out_size, activation_function = None):
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
-    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    biases = tf.Variable(tf.zeros([1, out_size])+0.1)
     Wx_plus_b = tf.matmul(inputs, Weights) + biases			# biases 和 结果 维数不同?
     if activation_function is None:
         outputs = Wx_plus_b
@@ -87,28 +82,42 @@ x = tf.placeholder(tf.float32, shape=[None,input_num], name="input")
 y = tf.placeholder(tf.float32, shape=[None,output_num], name = "label")
 
 l1 = add_layer(x, input_num, hid1, activation_function = tf.nn.sigmoid)
-# l2 = add_layer(l1, hid1, hid2, activation_function = tf.nn.sigmoid)
+# l2 = add_layer(l1, hid1, hid2, activation_function = tf.nn.relu)
 y_pred = add_layer(l1, hid1, output_num, activation_function = None)
 
 loss = tf.reduce_mean(tf.reduce_sum(tf.square(y_pred - y), reduction_indices = [1]))
 train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 
-init = tf.global_variables_initializer()
+saver = tf.train.Saver()
 
-sess = tf.Session()
-sess.run(init)
+def run_dp(origin_step, train_num):
+	init = tf.global_variables_initializer()
 
-# print(train_session_path)
-# print(train_graph_path)
+	sess = tf.Session()
+	sess.run(init)
 
-# if os.path.exists(train_graph_path) == True:
-# 	loader = tf.train.import_meta_graph(train_graph_path)
-# 	loader.restore(sess,tf.train.latest_checkpoint(train_session_folder))
+	models_folder = '../models/'
+	models_name = '1'
+	models_full_name = models_folder + models_name
+	models_graph_path = models_folder + models_name + '-' + str(origin_step) + '.meta'
 
-# saver = tf.train.Saver()
-# saver.save(sess, train_session_path, global_step=20)
+	print('models_graph_path: %s',models_graph_path)
+	if os.path.exists(models_graph_path) == True:
+		loader = tf.train.import_meta_graph(models_graph_path)
+		loader.restore(sess,tf.train.latest_checkpoint(models_folder))
+		new_step = origin_step + train_num
+	else:
+		new_step = train_num
 
-for step in range(20001):
-    sess.run(train_step, feed_dict = {x: X_data, y: Y_data})
-    if step % 200 == 0:
-        print(step, np.array([elem * 1992 for elem in sess.run(y_pred, feed_dict = {x: X_data, y: Y_data})]),  sess.run(loss, feed_dict = {x: X_data, y: Y_data}))
+	for step in range(train_num + 1):
+		sess.run(train_step, feed_dict = {x: X_data, y: Y_data})
+		if step % 1e8 == 0:
+			# learning_rate *= 0.95
+			# train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+			print(new_step - train_num + step, np.array([elem * 1992 for elem in sess.run(y_pred, feed_dict = {x: X_data, y: Y_data})]),  sess.run(loss, feed_dict = {x: X_data, y: Y_data}))
+
+	saver.save(sess, models_full_name, global_step = new_step)
+
+for i in range(1000):
+	epoch = 2000
+	run_dp(i * epoch, epoch)
